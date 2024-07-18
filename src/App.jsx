1f3d27de7c93aa3e1a2ab4e63b0f2667b003f89a
akiko-luka <ak.luka051@gmail.com>
2024-07-18 15:43:39 +0200

    add class for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return <div className="flex items-center justify-center text-7xl">App</div>;
 };
 
 export default App;
