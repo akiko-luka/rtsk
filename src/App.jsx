c2b852588a5bd294befce3520ab76b493eaf14be
Pilag6 <pilag6@gmail.com>
2024-07-18 16:00:01 +0200

    Update image in README.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="flex items-center justify-center text-7xl">App</div>;
+  return <div className="flex items-center justify-center text-7xl">React - TailwindCSS Starter Kit</div>;
 };
 
 export default App;
